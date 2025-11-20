49f2b4082c3fbff2bb2b26ccc3ba252a6faa9c5c
Hemalsha Adasooriya <hemalsha2022uni@gmail.com>
2025-11-20 18:14:25 +0530

    update

---
+++ game.js
@@ -0,0 +1,11 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+
+function nextSequence(){
+    var randomNumber = Math.random() * 4;
+    var randomChosenColour = buttonColours[Math.floor(randomNumber)];
+    gamePattern.push(randomChosenColour);
+}
+
+
+console.log(nextSequence());
