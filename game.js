df3d686fa74c1445edc6388f161363945c500fc5
Hemalsha Adasooriya <hemalsha2022uni@gmail.com>
2025-11-20 18:46:37 +0530

    add comment

--- game.js
+++ game.js
@@ -1,8 +1,13 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 
+//random color generator
 function nextSequence(){
     var randomNumber = Math.random() * 4;
     var randomChosenColour = buttonColours[Math.floor(randomNumber)];
     gamePattern.push(randomChosenColour);
+    console.log(gamePattern);
 }
+
+
+
