607042ae620be2dec0cc1454092ac6cb2769a4de
Hemalsha Adasooriya <hemalsha2022uni@gmail.com>
2025-11-22 18:43:39 +0530

    animation update

--- game.js
+++ game.js
@@ -3,13 +3,14 @@ var gamePattern = [];
 var userClickedPattern = [];
 
 
+//clicked button identifier
 $(".btn").click(function(){
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
-    console.log(userClickedPattern);
+    
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
 });
-
-
 
 
 //random color generator
@@ -20,7 +21,23 @@ function nextSequence(){
 
     $("#"+ randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    playSound(randomChosenColour);
+    animatePress(randomChosenColour);
+
+}
+
+//sound effect
+function playSound(name){
+    var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
+}
+
+//btn animation effect
+function animatePress(currentColour){
+    $("#" + currentColour).addClass("pressed");
+    
+    setTimeout(function() {
+    $("#" + currentColour).removeClass("pressed");
+}, 100);
 
 }
