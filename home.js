a55e62043abcde6cbea893e139854bd45354b404
Hemalsha Adasooriya <hemalsha2022uni@gmail.com>
2025-12-04 17:44:44 +0530

    Update color scheme for bottom text; change color array in home.js and adjust CSS for transition effect

--- home.js
+++ home.js
@@ -1,8 +1,9 @@
-const colors = ["red", "blue", "green", "yellow"];
+const colors = ["#19183B", "#708993", "#A1C2BD", "#0D1164"];
 let colorindex = 0;
 
 function changeColor() {
     $(".bottom-text").css("color", colors[colorindex]);
     colorindex = (colorindex + 1) % colors.length;
 }
+
 setInterval(changeColor, 1000);
